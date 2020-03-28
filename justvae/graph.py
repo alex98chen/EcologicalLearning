d3b40d29754ae106eeed0f80a9f46391a0bf252d
alex98chen <alex98chen@gmail.com>
2020-03-28 16:59:48 -0400

    added toy

---
+++ justvae/graph.py
@@ -0,0 +1,21 @@
+#%matplotlib inline 
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+import numpy as np
+
+fig = plt.figure()
+ax = plt.axes()
+
+loss_scores = np.load('loss_scores.npy')
+
+print(loss_scores.shape)
+
+x = np.linspace(0, 100, 11)
+print(x)
+print(loss_scores)
+for i in range(1,10):
+    plt.plot(x, loss_scores[i], label ='{}'.format(i))
+plt.title('loss scores for each number by percent of each number')
+plt.legend(loc='best')
+plt.savefig('lossByPercent.png')
+plt.show()
